ecbb33f54442282fa94a8fc4f20a8aa73f394955
JP Villamor <jpvillam@mailfence.com>
2022-02-05 13:01:20 -0500

    Procfile changes

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn basic.wsgi
+web: daphne rtgraph.asig:application --port $PORT --bind 0.0.0.0 -v2
+graphworker: python3 manage.py runworker --settings=rtgraph.settings -v2
--- rtgraph/asgi.py
+++ rtgraph/asgi.py
@@ -8,13 +8,15 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
+import django
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.routing import ProtocolTypeRouter, URLRouter, get_default_application
 from channels.auth import AuthMiddlewareStack
 from graph.routing import ws_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rtgraph.settings')
+django.setup()
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
     'websocket': AuthMiddlewareStack(URLRouter(ws_urlpatterns))
