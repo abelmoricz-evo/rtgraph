17b60d084ec7b8feafc561112e02c29b917d02fc
JP Villamor <jpvillam@mailfence.com>
2022-02-08 17:46:37 -0500

    added localhost to allowed hosts

--- rtgraph/settings.py
+++ rtgraph/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-rb+pbepu$nbye8=8^v9qu)r3$!0h9@1_zckt0+up3927f=m*ed
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1:8000', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1:8000', '127.0.0.1', 'localhost']
 
 
 # Application definition
