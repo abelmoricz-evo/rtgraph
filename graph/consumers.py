06676820f4b75b8fbdd7dde29c61d96eaa2bfebb
JP Villamor <jpvillam@mailfence.com>
2022-02-21 15:41:56 -0500

    fixed database

--- graph/consumers.py
+++ graph/consumers.py
@@ -1,6 +1,13 @@
 import json
 from random import randint
 from asyncio import sleep
+from asgiref.sync import sync_to_async
+
+from .models import User
+from .models import Sensor
+from .models import Record
+
+from . import counter
 
 from channels.generic.websocket import AsyncWebsocketConsumer
 
@@ -8,6 +15,15 @@ class GraphConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
 
+        NewUser = await User.create('marcellus wallace', 'wood', 'shotty McShotgun')
+        await NewUser.save_to_db()
+        ForceSensor = await Sensor.create('force-load', 'lbs', 50, NewUser)
+        await ForceSensor.save_to_db()
+
         for i in range(1000):
-            await self.send(json.dumps({'value': randint(-20,20)}))
+            data = counter.update()
+            NewRecord = await Record.create(i*200, data, NewUser.tool_selected, ForceSensor)
+            NewRecord.pk = None
+            await NewRecord.save_to_db()
+            await self.send(json.dumps({'value': data}))
             await sleep(.2)
