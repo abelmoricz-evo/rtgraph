c82ea3bc4fc4c257b35e2bc6e70fe05fd01f20d4
JP Villamor <jpvillam@mailfence.com>
2022-02-21 16:25:20 -0500

    asgi.py new line django_asgi_app

--- rtgraph/asgi.py
+++ rtgraph/asgi.py
@@ -11,6 +11,7 @@ import os
 import django
 
 from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
 from channels.routing import ProtocolTypeRouter, URLRouter, get_default_application
 from channels.auth import AuthMiddlewareStack
 from graph.routing import ws_urlpatterns
